e59c8343c4df08728996536fbc693206a97f07ad
Ono Oogami <ono@oogami.name>
2014-02-13 16:23:24 +0800

    improve fs watch

--- library/index.js
+++ library/index.js
@@ -263,13 +263,16 @@ var daemon = function(callback) {
 };
 
 
-var libraryChangedEventListener = function(event, filename) {
-	console.log('Library change detected. ', event, filename);
+var libraryChangedEventListener = function(event, filename, dirname) {
+	console.log('Library change detected. ', event, filename, dirname);
 };
 
 var addFsWatch = function(filepath, listener) {
 	console.log('Add watch for dir ' + filepath);
-	fs.watch(filepath, listener);
+	fs.watch(filepath, function(event, filename) {
+		if( !filename || filename.substr(0, 1) != '.' )
+			listener(event, filename, filepath);
+	});
 };
 
 var setupWatches =function(dir, listener, result_callback) {
--- test/watch.js
+++ test/watch.js
@@ -5,13 +5,16 @@ var async = require('async');
 
 // test watch recursively a huge HOME dir performance
 //
-var libraryChangedEventListener = function(event, filename) {
-	console.log('Library change detected. ', event, filename);
+var libraryChangedEventListener = function(event, filename, dirname) {
+	console.log('Library change detected. ', event, filename, dirname);
 };
 
 var addFsWatch = function(filepath, listener) {
 	console.log('Add watch for dir ' + filepath);
-	fs.watch(filepath, listener);
+	fs.watch(filepath, function(event, filename) {
+		if( !filename || filename.substr(0, 1) != '.' )
+			listener(event, filename, filepath);
+	});
 };
 
 var setupWatches =function(dir, listener, result_callback) {
